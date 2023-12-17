cc1adfd4841a89f6663bc5d6bd0cdaab56dbb37a
naseer12345 <naseer.khan00128@gmail.com>
2023-12-17 23:16:06 +0000

    just adding it to the github, nothing in here now

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const d = new Date();
 const dateString = d.toUTCString();
-const day = d.getDate()
 
-document.getElementById("kk").innerHTML = dateString;
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const month = months[d.getUTCMonth() ];
+const day = days[d.getDay()]
+const both = month + ", " + day
+document.getElementById("kk").innerHTML = both;
+
+
+document.querySelector(".mcbg").addEventListener("mousedown", function(){
+    document.querySelector('.img').style.width = '80%';
+    document.querySelector('.img').style.height = '80%';
+
+})
