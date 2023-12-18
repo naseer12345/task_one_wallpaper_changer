02785b33bf4c07514d1b0fead0bf4685e5758807
naseer12345 <naseer.khan00128@gmail.com>
2023-12-18 18:53:51 +0000

    with new changes

--- index.js
+++ index.js
@@ -9,9 +9,29 @@ const day = days[d.getDay()]
 const both = month + ", " + day
 document.getElementById("kk").innerHTML = both;
 
+const btnparentdiv = document.querySelector(".btndiv")
+const btn = document.createElement("button")
+btn.innerHTML="Customize"
 
-document.querySelector(".mcbg").addEventListener("mousedown", function(){
-    document.querySelector('.img').style.width = '80%';
-    document.querySelector('.img').style.height = '80%';
 
-})
+
+let holdTimer;
+
+const mcbgElement = document.querySelector(".mcbg");
+const imgElement = document.querySelector('.img'); // Make sure this is the correct selector
+// Define btnparentdiv and btn if they're used in your code
+
+mcbgElement.addEventListener("mousedown", function() {
+    // Start the timer when the mouse is held down
+    holdTimer = setTimeout(function() {
+        imgElement.style.width = '80%';
+        imgElement.style.height = '80%';
+        // Make sure btnparentdiv and btn are defined and used correctly here
+        // btnparentdiv.appendChild(btn);
+    }, 3000); // 3000 milliseconds = 3 seconds
+});
+
+mcbgElement.addEventListener("mouseup", function() {
+    // Clear the timer when the mouse is released
+    clearTimeout(holdTimer);
+});
